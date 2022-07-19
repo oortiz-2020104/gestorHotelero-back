2edf0ccd79d9024fc3b722a0d0d0d843fdf8625d
Osmar1n <oortiz-2020104@kinal.edu.gt>
2022-07-18 23:28:12 -0600

    Cambios para Heroku

--- src/controllers/hotel.controller.js
+++ src/controllers/hotel.controller.js
@@ -364,10 +364,10 @@ exports.uploadImageHotel = async (req, res) => {
                 const filePath = req.files.image.path; // \uploads\users\file_name.ext
 
                 //separar en jerarquía la ruta de la imágen (linux o MAC: ('\'))
-                const fileSplit = filePath.split('\\');// fileSplit = ['uploads', 'users', 'file_name.ext']
+                const fileSplit = filePath.split('/');// fileSplit = ['uploads', 'users', 'file_name.ext']
                 const fileName = fileSplit[2];// fileName = file_name.ext
 
-                const extension = fileName.split('\.'); // extension = ['file_name', 'ext']
+                const extension = fileName.split('.'); // extension = ['file_name', 'ext']
                 const fileExt = extension[1]; // fileExt = ext;
 
                 const validExt = await validateExtension(fileExt, filePath);
